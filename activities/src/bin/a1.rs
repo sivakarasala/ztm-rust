c20e1dd7544207d754ee776f9c34299050ede439
Siva Krishna <32276574+sivakarasala@users.noreply.github.com>
2024-07-01 18:07:19 +0000

    activity 1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,17 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Use a function to display your first name
+fn first_name() {
+    println!("Siva Krishna");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("Karasala");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
